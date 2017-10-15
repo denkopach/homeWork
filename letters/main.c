359250c64ce9c9eb1587776fd42976d331b5d68a
Den Kopach <32712833+denkopach@users.noreply.github.com>
2017-10-15 20:45:03 +0300

    ready

--- letters/main.c
+++ letters/main.c
@@ -12,6 +12,10 @@ int main()
         for (i = 0; i < 27; i++){
             if (letter[i] == userAnswer){
                 printf("\nNext letter is %c\n", letter[i+1]);
+                break;
+            }
+            if (i == 26) {
+                    printf("\nYou did not enter the letter of the Latin alphabet");
             }
         }
     }
