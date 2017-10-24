8668c289a4a2f82e94dd2533082816f6c13c5d8f
Den Kopach <denkopach@yahoo.com>
2017-10-24 22:20:26 +0300

    without malloc

--- countingOffLetters/main.c
+++ countingOffLetters/main.c
@@ -4,22 +4,21 @@
 
 int main()
 {
-    const int STRING_SIZE = 100; //the maximum size of the text to be typed
-    char textString [STRING_SIZE];
-    const int SIZE_ALPHABET = 26;
-    const int NUMBER_OF_COLUMNS = 2;
-    const int COLUMN_WITH_LETTER = 0;
-    const int COLUMN_WITH_LETTER_AMOUNT = 1;
-    char letterAndAmountArray [SIZE_ALPHABET][NUMBER_OF_COLUMNS];
-    char *tempVar; //temporary variable for text input
-    int temp = 0; //temporary variable for sorting
-    const int ASCII_NOMBER_SMAL_A = 97;
-    const int ASCII_NOMBER_BIG_A = 65;
-
-    int i,j = 0; //variables for counters
-    int sizeFilledArr = 0;//the size of the filled array
-
     for (;;){
+        const int STRING_SIZE = 100; //the maximum size of the text to be typed
+        char textString [STRING_SIZE];
+        const int SIZE_ALPHABET = 26;
+        const int NUMBER_OF_COLUMNS = 2;
+        const int COLUMN_WITH_LETTER = 0;
+        const int COLUMN_WITH_LETTER_AMOUNT = 1;
+        char letterAndAmountArray [SIZE_ALPHABET][NUMBER_OF_COLUMNS];
+        char *tempVar; //temporary variable for text input
+        int temp = 0; //temporary variable for sorting
+        const int ASCII_NOMBER_SMAL_A = 97;
+        const int ASCII_NOMBER_BIG_A = 65;
+
+        int i,j, n = 0; //variables for counters
+        int sizeFilledArr = 0;//the size of the filled array
 
         //zeroing an array
         for (i = 0; i < SIZE_ALPHABET; i++){
@@ -34,17 +33,20 @@ int main()
 
         //counting of letters
         for(i = 0; i < SIZE_ALPHABET; i++){
-            for(j = 0; j < strlen(textString); j++){
+            for(j = 0, n = 0; j < strlen(textString); j++){
+
                 if (textString[j] == i + ASCII_NOMBER_BIG_A || textString[j] == i + ASCII_NOMBER_SMAL_A){
                     letterAndAmountArray[sizeFilledArr][COLUMN_WITH_LETTER] = i + ASCII_NOMBER_BIG_A;
                     letterAndAmountArray[sizeFilledArr][1] = amountLetter;
                     amountLetter++;
+                    n = 1;
                 }
             }
             if (amountLetter > 1){
                 sizeFilledArr++;
             }
             amountLetter = 1;
+            n = 0;
         }
 
         //sort an array from large to small
