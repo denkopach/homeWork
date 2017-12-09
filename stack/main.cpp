99f3158188630c73ed77ce67023c69456808b53e
Den Kopach <denkopach@yahoo.com>
2017-12-09 11:54:40 +0200

    add comment and fix bugs

--- stack/main.cpp
+++ stack/main.cpp
@@ -5,12 +5,13 @@ using namespace std;
 class Stack{
 private:
     const int SIZE_QUEUE = 10;
-    int* queueArray = new int[SIZE_QUEUE];
+    int* queueArray;
     int* last;
     bool full;
 
 public:
     Stack(){
+        queueArray = new int[SIZE_QUEUE];
         last = queueArray;
     }
 
@@ -35,21 +36,28 @@ public:
         return data;
     }
 
+    //check for fullness
     bool isFull(){
         return last == &queueArray[10];
     }
+
+    //check for emptiness
     bool isVoid(){
         return last == queueArray;
+    }
+
+    ~Stack(){
+        delete []queueArray;
     }
 };
 
 int main()
 {
     Stack s;
-    for (int i = 0; i < 11; i++){
+    for (int i = 0; i < 12; i++){
         s.put(i);
     }
-    for (int i =0; i < 11; i++){
+    for (int i =0; i < 12; i++){
         cout<<s.get()<<endl;
     }
 }
